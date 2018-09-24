11f82d336acc3e8fa75df8619dc1360dbad7c405
RenaudBelloncle <renaud-belloncle@orange.fr>
2018-09-24 16:54:11 +0200

    Ajout __init__ to Model 1

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
 # coding=utf-8
-from Model1_GBP.Generator import Generator
-from Model1_GBP.Buffer import Buffer
-from Model1_GBP.Processor import Processor
-from Dictionary import Dictionary
-from Simulator import Simulator
 import matplotlib.pyplot as plt
+
+from Model1_GBP import simulator
 
 if __name__ == '__main__':
     print "Simulateur à évènements discrets de systèmes hybrides"
 
-    dictionary = Dictionary()
-
-    generator = Generator(dictionary)
-    buff = Buffer(dictionary)
-    processor = Processor(dictionary)
-
-    dictionary.add_link_component("job", buff)
-    dictionary.add_link_component("req", processor)
-    dictionary.add_link_component("done", buff)
-
-    simulator = Simulator(20, [generator, buff, processor])
     logs = simulator.run()
     plt.plot(logs[0], logs[1], drawstyle="steps-post")
     plt.show()
